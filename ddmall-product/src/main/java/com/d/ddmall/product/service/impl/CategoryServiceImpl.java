3cfd5b6b260964e9393eba83c15cb7fe0e7025fe
D <alvinsudi@163.com>
2020-11-12 21:59:32 +0800

    data数据的children为null

--- ddmall-product/src/main/java/com/d/ddmall/product/service/impl/CategoryServiceImpl.java
+++ ddmall-product/src/main/java/com/d/ddmall/product/service/impl/CategoryServiceImpl.java
@@ -32,18 +32,23 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
     @Override
     public List<CategoryEntity> listWithTree() {
-        //查出所有分类
+        //1、查出所有分类
         List<CategoryEntity> entities = baseMapper.selectList(null);
 
-        //组装成父子的树形结构
-        //找一级分类,父分类id为0
-        List<CategoryEntity> level1Menu = entities.stream()
-                .filter(categoryEntity -> categoryEntity.getParentCid() == 0)
-                .map((menu) -> {menu.setChildren(getChildrens(menu,entities)); return menu;})
-                .sorted((menu1,menu2)->{ return (menu1.getSort()==null?0:menu1.getSort()) - (menu2.getSort()==null?0:menu2.getSort());})
-                .collect(Collectors.toList());
+        //2、组装成父子的树形结构
 
-        return level1Menu;
+        //2.1）、找到所有的一级分类
+        List<CategoryEntity> level1Menus = entities.stream().filter(categoryEntity ->
+                categoryEntity.getParentCid() == 0
+        ).map((menu)->{
+            menu.setChildren(getChildrens(menu,entities));
+            return menu;
+        }).sorted((menu1,menu2)->{
+            return (menu1.getSort()==null?0:menu1.getSort()) - (menu2.getSort()==null?0:menu2.getSort());
+        }).collect(Collectors.toList());
+
+
+        return level1Menus;
     }
 
 //root 当前菜单 all子菜单
